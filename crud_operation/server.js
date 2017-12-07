e49bd8d01db2fb27e92abcbd775dd304e115c33b
prakash <mailmeprakash1992@gmail.com>
2017-12-07 18:57:54 +0530

    crud Opertion

--- crud_operation/server.js
+++ crud_operation/server.js
@@ -2,81 +2,120 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
 const Users = require('./schema.js')
 const app = express();
-const port     = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 
-mongoose.connect("mongodb://localhost/students", function(err) {
-       if (err) {
-       	console.log("db not connected")
-       }
-       console.log("db connected")
-	})
+mongoose.connect('mongodb://localhost/students', error => {
+	if (error) {
+		console.error('db not connected')
+	}
+
+	console.log('db connected')
+})
 
 
-
+/*
+ * Midleware..
+ */ 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
+/*
+ * Retrive All USers From DB
+ */ 
 
-app.post("/", (req, res) => {
-      let user = new Users()
-      user.name = req.body.name;
-      user.age = req.body.age;
-
-      user.save((err) => {
-      	if (err) {
-      		console.log("user insert failed ")
-      	}
-      	res.json({msg : "user saved"})
-      })
-})
-
-
-app.get("/allUsers", (req, res) => {
-	users.find((err, docs) => {
-		if (err) {
-			console.log("No Users")
-		}
-      if(docs){
-		res.json(docs)
-		}
-	})
-})
-
-
-app.put('/allUsers/:id', function(req, res) {
- let id = req.params.employee_id;
- var data = {
- name : req.body.name,
- salary : req.body.salary,
- age : req.body.age
- }
-  users.findByIdAndUpdate(id, data, function(err, doc) {
- if (err) throw err;
- res.json(doc);
- });
+app.get('/api/users/', (req, res, next) => {
+	Users.find({})
+	.then(users => res.json(users))
+	.catch(next);
 });
 
 
-app.delete('/allUsers/:id', function(req, res) {
-	let id = req.params.employee_id;
-	users.remove({
-		_id : id
-	}, function(err) {
-		if (err){
-			res.send(err);
-		}
-		else{
-			res.send('Successfully! Employee has been Deleted.');	
-		}
-	});
+/*
+ * Create New User
+ */ 
+
+app.post('/api/users/', (req, res, next) => {
+
+	const { name, age } = req.body || {};
+
+	if (!name || !age) {
+		const error = new Error('BadRequest');
+		return next(error);
+	}
+
+
+    const user = new Users({ name, age });
+
+	user.save()
+	.then(data => res.json(data))
+	.catch(next);
+
+})
+
+
+/*
+ * Update User By ID
+ */ 
+
+app.put('/api/users/:id', (req, res, next) => {
+	const { id } = req.params || {};
+
+	if (!id) {
+		const error = new Error('ID should Not Be Empty');
+		return next(error);
+	}
+
+	const { name, age } = req.body || {};
+	
+	const updateObj = {};
+
+	if (name) {
+		updateObj.name = name;
+	}
+
+	if (age) {
+		updateObj.age = age;
+	}
+
+
+	Users.findByIdAndUpdate(id, updateObj, {new: true})
+	.then(data => res.json(data))
+	.catch(next);
+});
+
+
+/*
+ * REmove User By ID
+ */ 
+
+app.delete('/api/users/:id', (req, res, next) => {
+	const { id } = req.params || {};
+
+	if (!id) {
+		const error = new Error('ID should Not Be Empty');
+		return next(error);
+	}
+
+	Users.findByIdAndRemove(id)
+	.then(data => res.json(data))
+	.catch(next);
 });
 
 
 
+/*
+ * Handle Errors
+ */ 
+
+app.use((error, req, res, next) => {
+	const { message } = error || {};
+	const isSuccess = false
+;	res.json({message, isSuccess})
+})
 
 
-app.listen(port, () => console.log("port connected " + port) )
+app.listen(port, () => console.log('port connected ' + port) )
